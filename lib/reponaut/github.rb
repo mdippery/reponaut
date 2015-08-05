c2f839af4d705980e5f9eaa7c37a7fe08db0bcc0
Michael Dippery <michael@monkey-robot.com>
2015-08-05 13:37:57 -0700

    Encapsulate repository info in class

--- lib/reponaut/github.rb
+++ lib/reponaut/github.rb
@@ -15,7 +15,21 @@ module Reponaut
       end
 
       def repos
-        JSON.parse(self.class.get("/users/#{username}/repos").body)
+        JSON.parse(self.class.get("/users/#{username}/repos").body).map { |e| Repository.new(e) }
+      end
+    end
+
+    class Repository
+      def initialize(data)
+        @data = data
+      end
+
+      def method_missing(symbol, *args)
+        if @data.include?(symbol.to_s)
+          @data[symbol.to_s]
+        else
+          super
+        end
       end
     end
   end
--- spec/reponaut/github_spec.rb
+++ spec/reponaut/github_spec.rb
@@ -14,13 +14,13 @@ module Reponaut
 
         it 'returns a valid repo name for a repo' do
           VCR.use_cassette('repos') do
-            expect(github.repos[0]['full_name']).to eq('mdippery/3ddv')
+            expect(github.repos[0].full_name).to eq('mdippery/3ddv')
           end
         end
 
         it 'returns a valid language for a repo' do
           VCR.use_cassette('repos') do
-            expect(github.repos[0]['language']).to eq('Java')
+            expect(github.repos[0].language).to eq('Java')
           end
         end
       end
