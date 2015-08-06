48927b7eb44010654a6e9c052ae12a6508b94165
Michael Dippery <michael@monkey-robot.com>
2015-08-06 13:35:20 -0700

    Respond to --version

--- lib/reponaut/application.rb
+++ lib/reponaut/application.rb
@@ -1,8 +1,16 @@
+require 'slop'
+require 'reponaut/version'
+
 module Reponaut
   class Application
     class << self
       def run
-        puts "Hello, world!"
+        opts = Slop.parse do |o|
+          o.on '--version' do
+            puts "reponaut, version #{Reponaut::VERSION}"
+            exit
+          end
+        end
       end
     end
   end
